7028d9f90cc15c845566b6eeb64022133c6ef8d2
Vankayala <S528870@nwmissouri.edu>
2017-10-06 16:19:17 -0500

    First commit

---
+++ app/src/main/java/s528870/funzone/MainActivity.java
@@ -0,0 +1,13 @@
+package s528870.funzone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
