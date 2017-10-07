e4c8c2858450000f415d6d47a0b63c5332e2e5bd
maheshvankayala <S528870@mail.nwmissouri.edu>
2017-10-06 20:57:03 -0500

    mahesh activities updated

---
+++ app/src/main/java/s528870/funzone/RemGuessNext.java
@@ -0,0 +1,13 @@
+package s528870.funzone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RemGuessNext extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rem_guess_next);
+    }
+}
